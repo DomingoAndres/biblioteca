df1a9fe3a722609be4ebedaf4c40d4829be9ddaf
Domingo Velazquez <domingoandresv98@gmail.com>
2025-04-02 21:10:13 -0300

    Quinto Commit se agergaron los libros

--- src/main/java/com/example/biblioteca/repository/LibroRepository.java
+++ src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -16,7 +16,13 @@ public class LibroRepository {
 
     @PostConstruct
     public void init(){
-        listalibros.add(new LibroModel(2,"978-1654565","Spring in action", "Addison-Wesley", 2018, "Pepito"));
+        listalibros.add(new LibroModel(2,"978-1654565","Spring in action", "Addison-Wesley", 2018, "Joshua Bosch"));
+
+        listalibros.add(new LibroModel(2, "978-1617294945", "Spring in Action", "Manning", 2020, "Craig Walls"));
+
+        listalibros.add(new LibroModel(3, "978-1491950357", "Designing Data-Intensive Applications", "O'Reilly Media", 2017, "Martin Kleppmann"));
+
+        listalibros.add(new LibroModel(4, "978-0132350884", "Clean Code", "Prentice Hall", 2008, "Robert C. Martin"));
     }
 
 }
